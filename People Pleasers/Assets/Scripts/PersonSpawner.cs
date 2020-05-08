0d73a5707d1acdcbc27b7e6069588505e85c9e5c
jhpp114 <jhpp114@uw.edu>
2020-05-08 04:57:02 -0700

    Added A button to indicate person spawn with number

--- People Pleasers/Assets/Scripts/PersonSpawner.cs
+++ People Pleasers/Assets/Scripts/PersonSpawner.cs
@@ -1,32 +1,85 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // to access btn with text
 
 public class PersonSpawner : MonoBehaviour
 {   
     public List<WaveInterface> waveInterfaces;
     private int startingWave = 0;
-    //private int generateRandomPerson;
+
+    private Button spawnButton;
+    private Text spawnNumberIndicator;
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        StartCoroutine(SpawnAllWaves());
+        // StartCoroutine(SpawnAllWaves());
+        spawnButton = GameObject.Find("SpawnButton").GetComponent<Button>();
+        spawnNumberIndicator = GameObject.Find("Path1Indicator").GetComponent<Text>();
+        SpawnButtonIndicator();
+        spawnNumberIndicator.text = "X" + waveInterfaces[0].GetNumberOfPerson();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+    
+    public void StartCoroutine()
+    {
+        StartCoroutine(SpawnAllWaves());
+        // I have to enable the button after the method above is called
+        spawnButton.enabled = false;
+        ButtonClicked();
+    }
+    
+    private void ButtonClicked()
+    {
+        spawnButton.gameObject.SetActive(false);
+    }
+    
+    private void SpawnButtonIndicator()
+    {
+        Debug.Log(waveInterfaces[0].GetPerson()[0]);
+        if (waveInterfaces[0].GetPerson()[0].name.Contains("Hungry"))
+        {
+            spawnButton.GetComponent<Image>().color = Color.red;
+        }
+        else if (waveInterfaces[0].GetPerson()[0].name.Contains("Thirsty"))
+        {
+            spawnButton.GetComponent<Image>().color = Color.blue;
+        }
+        else if (waveInterfaces[0].GetPerson()[0].name.Contains("Hot"))
+        {
+            spawnButton.GetComponent<Image>().color = new Color(255f, 174f, 0);
+        }
+        else if (waveInterfaces[0].GetPerson()[0].name.Contains("Bored"))
+        {
+            spawnButton.GetComponent<Image>().color = new Color(206f, 0, 255f);
+        }
+        else
+        {
+            Debug.Log("humm");
+        }
+    }
+
+
 
     private IEnumerator SpawnAllWaves()
     {
         for (var curWave = startingWave; curWave < waveInterfaces.Count; curWave++)
         {
             var current = waveInterfaces[curWave];
+            // for distinguish
+            // DistinguishPerson(current);
+
             yield return StartCoroutine(SpawnAllEnemiesByWave(current));
+            
         }
     }
+
+    
 
     private IEnumerator SpawnAllEnemiesByWave(WaveInterface _waveInterface)
     {
@@ -35,7 +88,6 @@ public class PersonSpawner : MonoBehaviour
             GeneratePerson(_waveInterface);
             yield return new WaitForSeconds(_waveInterface.GetTimeBetweenSpawns());
         }
-        
     }
     private void GeneratePerson(WaveInterface _waveInterface)
     {
