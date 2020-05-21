725126acee18add6ca4c85e02307a82968889ca5
Kirby490 <laha17014@gmail.com>
2020-05-20 17:47:50 -0700

    Completed tutorial Level 1 and 2

---
+++ People Pleasers/Assets/Scripts/TutorialGlow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialGlow : MonoBehaviour
+{
+    private float alpha = 0f;
+    private Color imageColor;
+    private bool isIncreasing = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        imageColor = GetComponent<Image>().color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isIncreasing)
+        {
+            alpha += Time.deltaTime;
+            if (alpha >= 0.9)
+            {
+                alpha = 0.9f;
+                isIncreasing = false;
+            }
+        }
+        else
+        {
+            alpha -= Time.deltaTime;
+            if (alpha <= 0)
+            {
+                alpha = 0f;
+                isIncreasing = true;
+            }
+        }
+        imageColor.a = alpha;
+        GetComponent<Image>().color = imageColor;
+    }
+
+    public void PointerEnter()
+    {
+        GameObject.Find("TutorialCanvas").GetComponent<TutorialController>().tutorialStep++;
+        gameObject.SetActive(false);
+    }
+}
---
+++ People Pleasers/Assets/TutorialController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    public int tutorialStep = 0;
+    private GameObject tutorialButton1;
+    private GameObject tutorialButton2;
+    private GameObject tutorialButton3;
+    private GameObject tutorialButton4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tutorialButton1 = GameObject.Find("TutorialButton1");
+        tutorialButton2 = GameObject.Find("TutorialButton2");
+        tutorialButton3 = GameObject.Find("TutorialButton3");
+        tutorialButton4 = GameObject.Find("TutorialButton4");
+        tutorialButton1.SetActive(true);
+        tutorialButton2.SetActive(false);
+        tutorialButton3.SetActive(false);
+        tutorialButton4.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (tutorialStep)
+        {
+            case 1:
+                tutorialButton2.SetActive(true);
+                break;
+            case 2:
+                tutorialButton3.SetActive(true);
+                break;
+            case 3:
+                tutorialButton4.SetActive(true);
+                break;
+        }
+    }
+}
