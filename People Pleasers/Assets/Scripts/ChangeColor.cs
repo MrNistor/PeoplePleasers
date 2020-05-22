768d9aed4093ebe8e81ea2abadaf07211eb89f6a
nhyoung <nhyoung@uw.edu>
2020-05-22 05:05:10 -0700

    add color fade on mouse hover for towers

---
+++ People Pleasers/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Color startColor;
+    public Color mouseOverColor;
+    public SpriteRenderer sprite;
+
+    void Start() {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseOver() {
+        Debug.Log("mouse over");
+        sprite.color = mouseOverColor;
+    }
+
+    void OnMouseExit() {
+        Debug.Log("mouse exit");
+        sprite.color = startColor;
+    }
+}
