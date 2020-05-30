7f2520a158b4dcd0687e7ec0c5a0d6c0e3dae33f
MrNistor <alexnistor.1999@gmail.com>
2020-05-29 18:55:36 -0700

    Added Upgrades

--- People Pleasers/Assets/Scripts/UIScript.cs
+++ People Pleasers/Assets/Scripts/UIScript.cs
@@ -6,14 +6,22 @@ using UnityEngine.UI;
 public class UIScript : MonoBehaviour
 {
     public GameObject ui_prefab;
-    private Button rangeButton;
+    public Button rangeButton;
+    public Button happyButton;
+    public GameObject range;
 
     // Start is called before the first frame update
     void Start()
     {
         ui_prefab = transform.GetChild(1).gameObject;
+        //rangeButton = ui_prefab.transform.Find("RangeButton").GetComponent<Button>();
+        rangeButton = ui_prefab.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Button>();
+        rangeButton.onClick.AddListener(UpgradeRange);
+        happyButton = ui_prefab.transform.GetChild(0).GetChild(0).GetChild(7).GetComponent<Button>();
+        happyButton.onClick.AddListener(UpgradeHappy);
+        range = this.transform.GetChild(0).gameObject;
         //ui_prefab = GameObject.Find("UI_Shop");
-       // ui_prefab.SetActive(false);
+        // ui_prefab.SetActive(false);
 
         //ui_prefab.gameObject.SetActive(false);
 
@@ -41,6 +49,43 @@ public class UIScript : MonoBehaviour
         {
             ui_prefab.gameObject.SetActive(true);
         }
+    }
+
+    void UpgradeRange()
+    {
+        range.transform.localScale *= 2;
+    }
+
+    void UpgradeHappy()
+    {
+        string towerName = this.name.Substring(0, name.Length - 11);
+        BurgerTower bTower;
+        FanTower fTower;
+        DrinkTower dTower;
+        EntertainTower eTower;
+
+
+        if (towerName == "Food")
+        {
+            bTower = range.GetComponent<BurgerTower>();
+            bTower.maxDistirubtionPower *= 2;
+        }
+        else if (towerName == "Fan")
+        {
+            fTower = range.GetComponent<FanTower>();
+            fTower.maxDistirubtionPower *= 2;
+        }
+        else if (towerName == "Drink")
+        {
+            dTower = range.GetComponent<DrinkTower>();
+            dTower.maxDistirubtionPower *= 2;
+        }
+        else if (towerName == "Entertainment")
+        {
+            eTower = range.GetComponent<EntertainTower>();
+            eTower.maxDistirubtionPower *= 2;
+        }
+        
     }
 
 }
