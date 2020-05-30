10ebcf13b5beb64b9bdb5564541e1b659009f0d4
nhyoung <nhyoung@uw.edu>
2020-05-29 23:23:55 -0700

    fix ui shop for entertainment

--- People Pleasers/Assets/Scripts/UIScript.cs
+++ People Pleasers/Assets/Scripts/UIScript.cs
@@ -7,10 +7,15 @@ public class UIScript : MonoBehaviour
 {
     public GameObject ui_prefab;
     private Button rangeButton;
+    public Color startColor;
+    public Color mouseOverColor;
+    public SpriteRenderer sprite;
 
     // Start is called before the first frame update
     void Start()
     {
+        sprite = GetComponent<SpriteRenderer>();
+
         ui_prefab = transform.GetChild(1).gameObject;
         //ui_prefab = GameObject.Find("UI_Shop");
        // ui_prefab.SetActive(false);
@@ -22,25 +27,35 @@ public class UIScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(1))
-        {
-            ui_prefab.gameObject.SetActive(true);
-        }
-        
+        //sprite = GetComponent<SpriteRenderer>();
     }
 
-    public void Clicked()
-    {
-        //ui_prefab.SetActive(true);
-        
+    void OnMouseDown() {
+        ui_prefab.gameObject.SetActive(true);
     }
 
-    private void OnMouseOver()
-    {
-        if (Input.GetMouseButtonDown(1))
-        {
-            ui_prefab.gameObject.SetActive(true);
-        }
+    void OnMouseExit() {
+        //Debug.Log("mouse exit");
+        sprite.color = startColor;
     }
 
+    void OnMouseOver() {
+        //Debug.Log("mouse over");
+        sprite.color = mouseOverColor;
+    }
+
+    // public void OnPointerEnter(PointerEventData pointerEventData)
+    // {
+    //     if (Input.GetMouseButtonDown(1))
+    //     {
+    //         ui_prefab.gameObject.SetActive(true);
+    //     }
+    // }
+
+    // //Detect when Cursor leaves the GameObject
+    // public void OnPointerExit(PointerEventData pointerEventData)
+    // {
+    //     //Output the following message with the GameObject's name
+    //     Debug.Log("Cursor Exiting ");
+    // }
 }
